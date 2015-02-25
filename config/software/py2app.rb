33438ad569a62f54f3853f0d691ccce1cceae330
Quentin Madec <quentin.madec@datadoghq.com>
2015-02-25 16:22:48 -0500

    [osx] install go and ship gohai

--- config/software/py2app.rb
+++ config/software/py2app.rb
@@ -8,4 +8,7 @@ env = {
 
 build do
   command "#{install_dir}/embedded/bin/pip install -U py2app", :env => env
+  # Required during the app build
+  command "cp /System/Library/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib"\
+          " #{install_dir}/embedded/lib/libpython2.7.dylib"
 end
