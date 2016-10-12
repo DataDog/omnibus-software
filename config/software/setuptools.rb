a31446a0f8787e5a63b3f578d699b722ce6baa22
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2016-10-12 13:44:30 -0400

    [pip] Upgrade to 8.1.2
    Also requires bump on pyro4 version and setuptools

--- config/software/setuptools.rb
+++ config/software/setuptools.rb
@@ -16,10 +16,9 @@
 #
 
 name "setuptools"
-default_version "0.7.7"
+default_version "28.3.0"
 
 dependency "python"
-
 
 relative_path "setuptools-#{version}"
 
@@ -32,10 +31,11 @@ if ohai['platform'] == 'windows'
     command "\"#{windows_safe_path(install_dir)}\\embedded\\python.exe\" ez_setup.py "
   end
 else
-  source :url => "https://pypi.python.org/packages/source/s/setuptools/setuptools-#{version}.tar.gz",
-         :md5 => '0d7bc0e1a34b70a97e706ef74aa7f37f'
+  source :url => "https://github.com/pypa/setuptools/archive/v#{version}.tar.gz",
+         :sha256 => '726d5247151b1be1fcc6b64bdb22df029364a75a4856a464ad88be65d285ab46'
   build do
     ship_license "PSFL"
+    command "#{install_dir}/embedded/bin/python bootstrap.py"
     command "#{install_dir}/embedded/bin/python setup.py install --prefix=#{install_dir}/embedded"
   end
 end
