74b8f79e6aba660c222c20cabcc2da5939a36675
Quentin Madec <quentin.madec@datadoghq.com>
2017-01-25 17:44:44 -0500

    [openssl] use Chef latest definition
    As it is theoretically compatible with both Windows and Unix.

--- config/software/cryptography.rb
+++ config/software/cryptography.rb
@@ -4,12 +4,8 @@ default_version "1.3.2"
 dependency "python"
 dependency "pip"
 
-if ohai["platform"] == "windows"
-  dependency "openssl-windows"
-else
-  dependency "libffi" # indirectly through the `cffi` python lib cryptography depends on
-  dependency "openssl"
-end
+dependency "libffi" # indirectly through the `cffi` python lib cryptography depends on
+dependency "openssl"
 
 build do
   ship_license "https://github.com/pyca/cryptography/blob/master/LICENSE.APACHE"
