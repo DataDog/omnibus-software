c82632746ba7343758111c7597ab01f585c085f5
Ofek Lev <ofekmeister@gmail.com>
2019-02-25 16:53:10 -0500

    fix

--- config/software/datadog-vsphere.rb
+++ config/software/datadog-vsphere.rb
@@ -5,6 +5,6 @@ dependency "python"
 dependency "pip"
 
 build do
-  wheel_name name.sub(/-/, "_")
+  wheel_name = name.sub(/-/, "_")
   pip "install https://dd-integrations-core-wheels-build-stable.s3.amazonaws.com/targets/simple/#{name}/#{wheel_name}-#{version}-py2.py3-none-any.whl"
 end
