a702efbd560f3ad80755a3a2ae4c16a1c638908f
Ofek Lev <ofekmeister@gmail.com>
2019-02-25 17:06:35 -0500

    Update datadog-vsphere.rb

--- config/software/datadog-vsphere.rb
+++ config/software/datadog-vsphere.rb
@@ -4,7 +4,8 @@ default_version "3.6.2"
 dependency "python"
 dependency "pip"
 
+wheel_name = name.sub(/-/, "_")
+
 build do
-  wheel_name = name.sub(/-/, "_")
   pip "install https://dd-integrations-core-wheels-build-stable.s3.amazonaws.com/targets/simple/#{name}/#{wheel_name}-#{version}-py2.py3-none-any.whl"
 end
