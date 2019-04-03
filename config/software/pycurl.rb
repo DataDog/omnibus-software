fd0c4a8c4ef405a2ba02519c1dea964394668ca6
Derek Brown <derek.brown@datadoghq.com>
2019-04-02 20:10:34 -0700

    change to new custom pycurl

--- config/software/pycurl.rb
+++ config/software/pycurl.rb
@@ -22,8 +22,8 @@ if ohai["platform"] != "windows"
   end
 else
   version "7.43.0"
-  wheel_name = "pycurl-7.43.0-cp27-none-win_amd64.whl"
-  wheel_md5 = "66c232b0da1e8314cf3794c5644ff49f"
+  wheel_name = "pycurl-7.43.0.2-cp27-cp27m-win_amd64.whl"
+  wheel_md5 = "25277be4928af13ea67a8a7b928e11a2"
 
   source :url => "https://s3.amazonaws.com/dd-agent-omnibus/#{wheel_name}",
          :md5 => wheel_md5
@@ -32,16 +32,5 @@ else
 
   build do
     pip "install #{wheel_name}"
-
-    # Delete these lines as soon as we have upgraded to pycurl > 7.43.0
-    # This is a custom built pycurl
-    python_lib_path = File.join(install_dir, "embedded", "Lib", "site-packages")
-    command "powershell.exe -Command wget -outfile pycurl.pyd https://s3.amazonaws.com/dd-agent-omnibus/pycurl.pyd"
-    pycurl_sha256 = "47c6b1caaa11a5a1cf3938a10bf32a21e8723c16ecbb4af8b820874dbe29ba31"
-    command "powershell -Command \"if ( $(CertUtil -hashfile pycurl.pyd sha256)[1] -replace \\\" \\\",\\\"\\\" | grep \"#{pycurl_sha256}\" ) { mv pycurl.pyd #{python_lib_path} -force } else { exit 1 } \" "
-
-    command "powershell.exe -Command wget -outfile msvcr110.dll https://s3.amazonaws.com/dd-agent-omnibus/msvcr110.dll"
-    vcruntime_sha256 = "0cbbd9691f08434da3617874f99c6dd87538cbd65b5d8bc39fce378d4ed29eed"
-    command "powershell -Command \"if ( $(CertUtil -hashfile msvcr110.dll sha256)[1] -replace \\\" \\\",\\\"\\\" | grep \"#{vcruntime_sha256}\" ) { mv msvcr110.dll #{python_lib_path} -force } else { exit 1 } \" "
   end
 end
