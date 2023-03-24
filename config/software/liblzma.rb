48d9436c37bf5dfeeb38434c1fc1c9730abc479e
David du Colombier <djc@datadoghq.com>
2023-03-24 15:24:22 +0100

    [liblzma] Replace with upstream version

--- config/software/liblzma.rb
+++ config/software/liblzma.rb
@@ -1,6 +1,5 @@
 #
-# Copyright:: Copyright (c) 2014 Chef Software, Inc.
-# License:: Apache License, Version 2.0
+# Copyright 2014-2018 Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,27 +15,47 @@
 #
 
 name "liblzma"
-default_version "5.0.5"
+default_version "5.2.10"
 
-source url: "https://tukaani.org/xz/xz-#{version}.tar.gz",
-       sha256: "5dcffe6a3726d23d1711a65288de2e215b4960da5092248ce63c99d50093b93a"
+license "Public-Domain"
+license_file "COPYING"
+skip_transitive_dependency_licensing true
+
+# version_list: url=http://tukaani.org/xz/ filer=*.tar.gz
+
+version("5.2.10") { source sha256: "eb7a3b2623c9d0135da70ca12808a214be9c019132baaa61c9e1d198d1d9ded3" }
+version("5.2.7") { source sha256: "06327c2ddc81e126a6d9a78b0be5014b976a2c0832f492dcfc4755d7facf6d33" }
+version("5.2.6") { source sha256: "a2105abee17bcd2ebd15ced31b4f5eda6e17efd6b10f921a01cda4a44c91b3a0" }
+version("5.2.5") { source sha256: "f6f4910fd033078738bd82bfba4f49219d03b17eb0794eb91efbae419f4aba10" }
+version("5.2.4") { source sha256: "b512f3b726d3b37b6dc4c8570e137b9311e7552e8ccbab4d39d47ce5f4177145" }
+version("5.2.3") { source sha256: "71928b357d0a09a12a4b4c5fafca8c31c19b0e7d3b8ebb19622e96f26dbf28cb" }
+version("5.2.2") { source sha256: "73df4d5d34f0468bd57d09f2d8af363e95ed6cc3a4a86129d2f2c366259902a2" }
+
+source url: "https://tukaani.org/xz/xz-#{version}.tar.gz"
 
 relative_path "xz-#{version}"
 
 build do
-  license "Public-Domain"
+  env = with_standard_compiler_flags(with_embedded_path)
+  # liblzma properly uses CFLAGS for C compilation and CPPFLAGS for common
+  # flags used across tools such as windres.  Don't put anything in it
+  # that can be misinterpreted by windres.
+  env["CPPFLAGS"] = "-I#{install_dir}/embedded/include" if windows?
 
-  cmd = [ "./configure",
-          "--prefix=#{install_dir}/embedded",
-          "--disable-debug",
-          "--disable-dependency-tracking"].join(" ")
+  config_command = [
+    "--disable-debug",
+    "--disable-dependency-tracking",
+    "--disable-doc",
+    "--disable-scripts",
+    "--disable-lzma-links",
+    "--disable-lzmainfo",
+    "--disable-lzmadec",
+    "--disable-xzdec",
+    "--disable-xz",
+  ]
+  config_command << "--disable-nls" if windows?
 
-  env = {
-    "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-    "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-    "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
-  }
+  configure(*config_command, env: env)
 
-  command cmd, env: env
-  command "make install", env: env
+  make "install", env: env
 end
