3c176d287038b70abf922e07f92ce81a853eda23
Alex Lopez <alex.lopez.zorzano@gmail.com>
2024-09-12 12:26:14 +0200

    Fix Python build dependencies (#542)
    * Build wide-character ncurses after non-wide-character
    
    So that header files under include reflect wide character support
    
    * Only build wide-char, add pkg-config files
    
    * bzip2: Add a pkgconfig file and fix install-name in patch

--- config/software/ncurses.rb
+++ config/software/ncurses.rb
@@ -33,21 +33,6 @@ relative_path "ncurses-#{version}"
 env = with_embedded_path
 env = with_standard_compiler_flags(env, aix: { use_gcc: true })
 
-########################################################################
-#
-# wide-character support:
-# Ruby 1.9 optimistically builds against libncursesw for UTF-8
-# support. In order to prevent Ruby from linking against a
-# package-installed version of ncursesw, we build wide-character
-# support into ncurses with the "--enable-widec" configure parameter.
-# To support other applications and libraries that still try to link
-# against libncurses, we also have to create non-wide libraries.
-#
-# The methods below are adapted from:
-# http://www.linuxfromscratch.org/lfs/view/development/chapter06/ncurses.html
-#
-########################################################################
-
 build do
   license "MIT"
   license_file "https://gist.githubusercontent.com/remh/41a4f7433c77841c302c/raw/d15db09a192ca0e51022005bfb4c3a414a996896/ncurse.LICENSE"
@@ -66,6 +51,7 @@ build do
     "--without-cxx-binding",
     "--enable-overwrite",
     "--enable-widec",
+    "--enable-pc-files",
     "--without-manpages",
     "--without-tests",
   ]
@@ -75,25 +61,8 @@ build do
   command "make -j #{workers}", env: env
   command "make -j #{workers} install", env: env
 
-  # build non-wide-character libraries
-  command "make distclean"
-  configure_options = [
-    "--with-shared",
-    "--disable-static",
-    "--with-termlib",
-    "--without-debug",
-    "--without-normal",
-    "--without-cxx-binding",
-    "--enable-overwrite",
-  ]
-  configure_options << "--with-libtool" if ohai["platform"] == "aix"
-  configure(*configure_options, env: env)
-  command "make -j #{workers}", env: env
-
-  # installing the non-wide libraries will also install the non-wide
-  # binaries, which doesn't happen to be a problem since we don't
-  # utilize the ncurses binaries in private-chef (or oss chef)
-  command "make -j #{workers} install", env: env
+  # Python's detection seems to expect to fiind a ncurses.pc rather than ncursesw.pc
+  link "#{install_dir}/embedded/lib/pkgconfig/ncursesw.pc", "#{install_dir}/embedded/lib/pkgconfig/ncurses.pc"
 
   # Ensure embedded ncurses wins in the LD search path
   if ohai["platform"] == "smartos"
